e13cbb43e028fcfdc2dad11f635b37234a20d4c3
Shuai YUAN <yszheda@gmail.com>
2017-06-02 16:23:39 +0800

    Use ndk-build instead

---
+++ jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)/..
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := neon
+LOCAL_SRC_FILES := neon.c
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_CFLAGS := -mfloat-abi=softfp -mfpu=neon
+# LOCAL_LDLIBS := -lrt
+# LOCAL_LDFLAGS := -lrt
+
+TARGET_PLATFORM := android-20
+TARGET_ARCH_ABI := armeabi-v7a
+# LOCAL_ARM_MODE := arm
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)    
+	LOCAL_ARM_NEON := true    
+endif
+
+
+# include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_EXECUTABLE)
+
+####################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := serial
+LOCAL_SRC_FILES := serial.c
+include $(BUILD_EXECUTABLE)
---
+++ jni/Application.mk
@@ -0,0 +1,5 @@
+APP_PROJECT_PATH := $(call my-dir)/..
+APP_PLATFORM := android-10    
+APP_STL := stlport_static
+APP_ABI := armeabi-v7a
+APP_CPPFLAGS += -fexceptions
