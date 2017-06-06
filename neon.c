bbcb46be7506528ba5b33da36e298ce9f620bf97
Shuai YUAN <yszheda@gmail.com>
2017-06-06 17:36:18 +0800

    Reorganize operations to avoid data hazards

--- neon.c
+++ neon.c
@@ -119,7 +119,7 @@ static inline unsigned int get_cyclecount (void)
 
 // https://en.wikipedia.org/wiki/YUV
 // Full swing for BT.601
-void RGB2YUV_NEON(unsigned char * __restrict__ yuv, unsigned char * __restrict__ rgb, int pixel_num)
+void RGB2YUV_NEON_gold(unsigned char * __restrict__ yuv, unsigned char * __restrict__ rgb, int pixel_num)
 {
     const uint8x8_t u8_zero = vdup_n_u8(0);
     const int8x8_t s8_zero = vdup_n_s8(0);
@@ -133,6 +133,8 @@ void RGB2YUV_NEON(unsigned char * __restrict__ yuv, unsigned char * __restrict__
     for (i = 0; i < count; ++i) {
         // Load rgb
         uint8x16x3_t pixel_rgb = vld3q_u8(rgb);
+
+        rgb += 3 * 16;
 
         uint8x8_t high_r = vget_high_u8(pixel_rgb.val[0]);
         uint8x8_t low_r = vget_low_u8(pixel_rgb.val[0]);
@@ -349,6 +351,130 @@ void RGB2YUV_NEON(unsigned char * __restrict__ yuv, unsigned char * __restrict__
         print_s8x16(v);
         print_u8x16(pixel_yuv.val[2]);
 #endif
+
+        // Store
+        vst3q_u8(yuv, pixel_yuv);
+        yuv += 3 * 16;
+    }
+
+    // Handle leftovers
+    for (i = count * 16; i < pixel_num; ++i) {
+        uint8_t r = rgb[i * 3];
+        uint8_t g = rgb[i * 3 + 1];
+        uint8_t b = rgb[i * 3 + 2];
+
+        uint16_t y_tmp = 76 * r + 150 * g + 29 * b;
+        int16_t u_tmp = -43 * r - 84 * g + 127 * b;
+        int16_t v_tmp = 127 * r - 106 * g - 21 * b;
+
+        y_tmp = (y_tmp + 128) >> 8;
+        u_tmp = (u_tmp + 128) >> 8;
+        v_tmp = (v_tmp + 128) >> 8;
+
+        yuv[i * 3] = (uint8_t) y_tmp;
+        yuv[i * 3 + 1] = (uint8_t) (u_tmp + 128);
+        yuv[i * 3 + 2] = (uint8_t) (v_tmp + 128);
+    }
+}
+
+void RGB2YUV_NEON(unsigned char * __restrict__ yuv, unsigned char * __restrict__ rgb, int pixel_num)
+{
+    const uint8x8_t u8_zero = vdup_n_u8(0);
+    const uint16x8_t u16_rounding = vdupq_n_u16(128);
+    const int16x8_t s16_rounding = vdupq_n_s16(128);
+    const int8x16_t s8_rounding = vdupq_n_s8(128);
+
+    int count = pixel_num / 16;
+
+    int i;
+    for (i = 0; i < count; ++i) {
+        // Load rgb
+        uint8x16x3_t pixel_rgb = vld3q_u8(rgb);
+
+        uint8x8_t high_r = vget_high_u8(pixel_rgb.val[0]);
+        uint8x8_t low_r = vget_low_u8(pixel_rgb.val[0]);
+        uint8x8_t high_g = vget_high_u8(pixel_rgb.val[1]);
+        uint8x8_t low_g = vget_low_u8(pixel_rgb.val[1]);
+        uint8x8_t high_b = vget_high_u8(pixel_rgb.val[2]);
+        uint8x8_t low_b = vget_low_u8(pixel_rgb.val[2]);
+        int16x8_t signed_high_r = vreinterpretq_s16_u16(vaddl_u8(high_r, u8_zero));
+        int16x8_t signed_low_r = vreinterpretq_s16_u16(vaddl_u8(low_r, u8_zero));
+        int16x8_t signed_high_g = vreinterpretq_s16_u16(vaddl_u8(high_g, u8_zero));
+        int16x8_t signed_low_g = vreinterpretq_s16_u16(vaddl_u8(low_g, u8_zero));
+        int16x8_t signed_high_b = vreinterpretq_s16_u16(vaddl_u8(high_b, u8_zero));
+        int16x8_t signed_low_b = vreinterpretq_s16_u16(vaddl_u8(low_b, u8_zero));
+
+        // NOTE:
+        // declaration may not appear after executable statement in block
+        uint16x8_t high_y;
+        uint16x8_t low_y;
+        uint8x8_t scalar = vdup_n_u8(76);
+        int16x8_t high_u;
+        int16x8_t low_u;
+        int16x8_t signed_scalar = vdupq_n_s16(-43);
+        int16x8_t high_v;
+        int16x8_t low_v;
+        uint8x16x3_t pixel_yuv;
+        int8x16_t u;
+        int8x16_t v;
+
+        // 1. Multiply transform matrix (Y′: unsigned, U/V: signed)
+        high_y = vmull_u8(high_r, scalar);
+        low_y = vmull_u8(low_r, scalar);
+
+        high_u = vmulq_s16(signed_high_r, signed_scalar);
+        low_u = vmulq_s16(signed_low_r, signed_scalar);
+
+        signed_scalar = vdupq_n_s16(127);
+        high_v = vmulq_s16(signed_high_r, signed_scalar);
+        low_v = vmulq_s16(signed_low_r, signed_scalar);
+
+        scalar = vdup_n_u8(150);
+        high_y = vmlal_u8(high_y, high_g, scalar);
+        low_y = vmlal_u8(low_y, low_g, scalar);
+
+        signed_scalar = vdupq_n_s16(-84);
+        high_u = vmlaq_s16(high_u, signed_high_g, signed_scalar);
+        low_u = vmlaq_s16(low_u, signed_low_g, signed_scalar);
+
+        signed_scalar = vdupq_n_s16(-106);
+        high_v = vmlaq_s16(high_v, signed_high_g, signed_scalar);
+        low_v = vmlaq_s16(low_v, signed_low_g, signed_scalar);
+
+        scalar = vdup_n_u8(29);
+        high_y = vmlal_u8(high_y, high_b, scalar);
+        low_y = vmlal_u8(low_y, low_b, scalar);
+
+        signed_scalar = vdupq_n_s16(127);
+        high_u = vmlaq_s16(high_u, signed_high_b, signed_scalar);
+        low_u = vmlaq_s16(low_u, signed_low_b, signed_scalar);
+
+        signed_scalar = vdupq_n_s16(-21);
+        high_v = vmlaq_s16(high_v, signed_high_b, signed_scalar);
+        low_v = vmlaq_s16(low_v, signed_low_b, signed_scalar);
+        // 2. Scale down (">>8") to 8-bit values with rounding ("+128") (Y′: unsigned, U/V: signed)
+        // 3. Add an offset to the values to eliminate any negative values (all results are 8-bit unsigned)
+
+        high_y = vaddq_u16(high_y, u16_rounding);
+        low_y = vaddq_u16(low_y, u16_rounding);
+
+        high_u = vaddq_s16(high_u, s16_rounding);
+        low_u = vaddq_s16(low_u, s16_rounding);
+
+        high_v = vaddq_s16(high_v, s16_rounding);
+        low_v = vaddq_s16(low_v, s16_rounding);
+
+        pixel_yuv.val[0] = vcombine_u8(vqshrn_n_u16(low_y, 8), vqshrn_n_u16(high_y, 8));
+
+        u = vcombine_s8(vqshrn_n_s16(low_u, 8), vqshrn_n_s16(high_u, 8));
+
+        v = vcombine_s8(vqshrn_n_s16(low_v, 8), vqshrn_n_s16(high_v, 8));
+
+        u = vaddq_s8(u, s8_rounding);
+        pixel_yuv.val[1] = vreinterpretq_u8_s8(u);
+
+        v = vaddq_s8(v, s8_rounding);
+        pixel_yuv.val[2] = vreinterpretq_u8_s8(v);
 
         // Store
         vst3q_u8(yuv, pixel_yuv);
